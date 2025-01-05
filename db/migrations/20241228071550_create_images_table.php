b9f9b2a95c5ffb5002a9c315e15b1530e896b387
pawsitive-dav <rhea.captain@gmail.com>
2025-01-05 11:31:08 +0700

    Update 20241228071550_create_images_table.php

--- db/migrations/20241228071550_create_images_table.php
+++ db/migrations/20241228071550_create_images_table.php
@@ -8,8 +8,9 @@ final class CreateImagesTable extends AbstractMigration
 {
     public function change(): void
     {
-        $table = $this->table('images', ['id' => 'image_id']);
-        $table->addColumn('group', 'string', ['limit' => 255, 'null' => true])
+        // สร้างตาราง images
+        $imagesTable = $this->table('images', ['id' => 'image_id']);
+        $imagesTable->addColumn('group', 'string', ['limit' => 255, 'null' => true])
             ->addColumn('name', 'string', ['limit' => 255, 'null' => true])
             ->addColumn('path', 'string', ['limit' => 512, 'null' => true])
             ->addColumn('base_url', 'text', ['null' => true])
@@ -19,12 +20,12 @@ final class CreateImagesTable extends AbstractMigration
             ->addColumn('uploaded_by', 'integer', ['null' => true, 'signed' => false])
             ->addColumn('uploaded_at', 'timestamp', [
                 'default' => 'CURRENT_TIMESTAMP',
-                'null' => true,
+                'null' => false,
             ])
             ->addColumn('updated_at', 'timestamp', [
                 'default' => 'CURRENT_TIMESTAMP',
                 'update' => 'CURRENT_TIMESTAMP',
-                'null' => true,
+                'null' => false,
             ])
             ->addIndex(['uploaded_by'], ['name' => 'idx_uploaded_by'])
             ->create();
