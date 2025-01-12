baaa22b4ce7aca3d8ba8d0d7944e25fda1decf77
pawsitive-dav <rhea.captain@gmail.com>
2025-01-12 21:20:19 +0700

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,10 +1,11 @@
 <?php
 
+use Dotenv\Dotenv;
 use Slim\Factory\AppFactory;
 use App\Database\Connection;
-use Dotenv\Dotenv;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Server\RequestHandlerInterface as RequestHandler;
+use Psr\Http\Message\ResponseInterface as Response;
 
 require __DIR__ . '/../vendor/autoload.php';
 
@@ -13,15 +14,24 @@ $dotenv->load();
 
 $app = AppFactory::create();
 
-$app->add(function (Request $request, RequestHandler $handler) {
+Connection::initialize();
+
+$app->add(function (Request $request, RequestHandler $handler): Response {
+    if ($request->getMethod() === 'OPTIONS') {
+        $response = new \Slim\Psr7\Response();
+        return $response
+            ->withHeader('Access-Control-Allow-Origin', '*')
+            ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+            ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS')
+            ->withStatus(200);
+    }
+
     $response = $handler->handle($request);
     return $response
         ->withHeader('Access-Control-Allow-Origin', '*')
         ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
         ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
 });
-
-Connection::initialize();
 
 $routes = require __DIR__ . '/../src/Routes/index.php';
 $routes($app);
